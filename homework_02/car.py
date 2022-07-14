9daf506028c33669e4bf27adc5e53d87b16607b7
i.kartashov <i.kartashov@yadro.com>
2022-07-15 02:02:16 +0300

    delete commented code

--- homework_02/car.py
+++ homework_02/car.py
@@ -6,11 +6,6 @@ from homework_02.engine import Engine
 
 
 class Car(Vehicle):
-    # def __init__(self, weight, fuel, fuel_consumption):
-    #     self.weight = weight
-    #     self.fuel = fuel
-    #     self.fuel_consumption = fuel_consumption
-
     engine = None
 
     def set_engine(self, engine: Engine):
